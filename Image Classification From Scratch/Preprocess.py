76c4e5b46e7dbfd639deffde96597f398564a590
jayrajput <62384561+RajputJay41@users.noreply.github.com>
2020-11-25 17:48:41 +0530

    Add files via upload

---
+++ Image Classification From Scratch/Preprocess.py
@@ -0,0 +1,28 @@
+#Filter out corrupted images
+#When working with lots of real-world image data, corrupted images are a common occurence. 
+#Let's filter out badly-encoded images that do not feature the string "JFIF" in their header.
+
+
+
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+num_skipped = 0
+for folder_name in ("Cat", "Dog"):
+    folder_path = os.path.join("PetImages", folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+print("Deleted %d images" % num_skipped)
